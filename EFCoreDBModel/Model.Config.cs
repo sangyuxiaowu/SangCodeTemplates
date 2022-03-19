ea9bf732791873f25dfb68758cdc086b08aa4d7d
sangsq <sang93@qq.com>
2022-03-19 23:30:40 +0800

    添加项目文件。

---
+++ EFCoreDBModel/Model.Config.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace $rootnamespace$
+{
+	public class $safeitemrootname$Config : IEntityTypeConfiguration<$safeitemrootname$>
+	{
+		public void Configure(EntityTypeBuilder<$safeitemrootname$> builder)
+		{
+			//builder.ToTable("");
+		}
+	}
+}
